541afc88cde2d8023f910ee9f352c6656edd7c2a
Biruk-Tassew <biruktassew21@gmail.com>
2023-09-16 00:32:30 +0300

    feat(setup-backen): setup the folder structure

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, {
+    Application,
+    Request,
+    Response,
+    NextFunction
+  } from 'express'
+  
+  import routes from "./routes";
+  import errorHandler from "./middlewares/ErrorHandler";
+  import cookieParser from "cookie-parser";
+  import expressupload from "express-fileupload";
+  import cors from "cors";
+  
+  const app: Application = express()
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(expressupload());
+  app.use(cors());
+  
+  
+  app.get("/", (req, res) => {
+    return res.send("Welcome to AFRO Class!");
+  });
+  
+  // Error handling
+  app.use(errorHandler);
+  
+  export const config = {
+    api: {
+      timeout: 30
+    }
+  };
+  
+  export default app;
+  
